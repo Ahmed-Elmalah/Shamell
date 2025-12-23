320cbfb827a4194ca5dab14f657c5b8467a963c4
ahmed-elmalah <o1027352352@gmail.com>
2025-12-23 21:22:45 +0200

    apply main color

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 
 export default function NavBar() {
   return (
-    <div className=" h-18.75 bg-[#1C1C1C] rounded-2xl sticky top-2.5 z-50 ">
+    <div className=" h-18.75 bg-main-gr rounded-2xl sticky top-2.5 z-50 ">
         
     </div>
   )
