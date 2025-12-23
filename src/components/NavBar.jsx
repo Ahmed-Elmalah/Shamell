d65a2bf23ac17cb89c946b2d6d68d423a6a769cd
ahmed-elmalah <o1027352352@gmail.com>
2025-12-23 22:03:41 +0200

    full navbar without responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,49 @@
-
+import logo from '../assets/Logo.png'
+import ThemeToggle from './themeToggle/ThemeToggle'
 export default function NavBar() {
   return (
-    <div className=" h-18.75 bg-main-gr rounded-2xl sticky top-2.5 z-50 ">
+    <nav className="h-[75px] gap-[70px] bg-[#1C1C1C] rounded-2xl sticky top-2.5 z-50 flex items-center justify-between px-8 text-white mx-4">
+      
+      {/* 1. اللوجو على الشمال */}
+      <div className="flex items-center">
+        <img 
+          src={logo} // حط مسار صورة "شامل" هنا
+          alt="Shamel Logo" 
+          className="h-[51px] w-[137]  object-contain"
+        />
+      </div>
+
+      {/* 2. القائمة اللي في النص */}
+      <ul className="hidden md:flex items-center gap-x-8 text-sm font-medium text-gray-300">
+        <li className="hover:text-white cursor-pointer text-[18px] transition-colors">Home</li>
+        <li className="hover:text-white cursor-pointer text-[18px] transition-colors">About</li>
+        <li className="hover:text-white cursor-pointer text-[18px] transition-colors">Our Courses</li>
+        <li className="hover:text-white cursor-pointer text-[18px] transition-colors">Code Locations</li>
+        <li className="hover:text-white cursor-pointer text-[18px] transition-colors">Questions</li>
+      </ul>
+
+      {/* 3. الجزء اللي على اليمين (Sign Up, Login, Language, Toggle) */}
+      <div className="flex items-center gap-x-8">
         
-    </div>
+        {/* Sign Up بـ Gradient text */}
+        <button className="cursor-pointer text-[18px] font-semibold bg-main-gr bg-clip-text text-transparent hover:opacity-80 transition-opacity">
+          Sign Up
+        </button>
+
+        {/* Login بـ Gradient background */}
+        <button className="cursor-pointer bg-main-gr w-24 h-11.5 rounded-md text-[18px] font-bold hover:scale-103 transition-all">
+          Login
+        </button>
+
+        {/* زر تغيير اللغة */}
+        <button className="text-[18px] font-bold hover:text-blue-400 transition-colors">
+          ع
+        </button>
+
+        {/* Dark Mode Toggle شكل بسيط */}
+        <ThemeToggle/>
+
+      </div>
+    </nav>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ export default function
   return (
     <div className="w-full px-17.75 py-2.5 relative">
         <NavBar/>
-        <div className=" h-[200vh] bg-red-500"></div>
+        {/* <div className=" h-[200vh] bg-red-500"></div> */}
         
     </div>
   )
