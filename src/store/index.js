6d4382515e94a4b75b78d794196605f19620aa17
ahmed-elmalah <o1027352352@gmail.com>
2025-12-25 10:58:33 +0200

    create store and useTheme

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+
+export const useTheme = create((set)=>({
+    isDarkMode : true,
+
+    toggleTheme : ()=>(set((state)=>{
+        const nextMode = !state.isDarkMode;
+        if(nextMode){
+            document.documentElement.classList.add('dark')
+        }else{
+            document.documentElement.classList.remove('dark')
+
+        }
+
+        return {isDarkMode}
+    })),
+
+    initTheme : ()=>{
+        const isDark = document.documentElement.classList.contains('dark') || true;
+        if(isDark) document.documentElement.classList.add('dark');
+        set({isDarkMode : isDark});
+    }
+    
+}))
