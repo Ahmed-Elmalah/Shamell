4de26d03bcbce0d42b151526cf04b0d3e452f247
Mazm-2100 <mazmgoo@gmail.com>
2025-12-25 15:29:46 +0200

    OurCoursesResponsive

--- src/Sections/OurCourses.jsx
+++ src/Sections/OurCourses.jsx
@@ -2,6 +2,8 @@ import OurCoursesCard from "../components/OurCoursesCard";
 import img1 from "../assets/OurCorsesImg1.jpg";
 import img2 from "../assets/OurCorsesImg2.jpg";
 import img3 from "../assets/OurCorsesImg3.jpg";
+import { GrFormNextLink } from "react-icons/gr";
+
 export default function OurCourses() {
   const cards = [
     {
@@ -54,17 +56,20 @@ export default function OurCourses() {
     },
   ];
   return (
-    <div id="courses" className="w-full  flex flex-col gap-2 items-center">
-      <h3 className="text-[36px] font-semibold">Our Courses</h3>
-      <p className="text-[18px]">
+    <div id="courses" className="w-full  flex flex-col gap-3 items-center justify-center">
+      <h3 className="text-[36px] dark:text-white text-black font-semibold">Our Courses</h3>
+      <p className="text-[16px] dark:text-white text-[#121212ce] text-center">
         Explore our top Courses curated by experts to boost your learning
         journey
       </p>
-      <div className="w-full h-[90%] p-4 gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center">
+      <div className="w-full h-[90%] px-4 py-6 gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center">
         {cards.map((el, index) => (
           <OurCoursesCard key={index} el={el} />
         ))}
       </div>
+      <button className="btn border-0 w-[138px] h-[48px] shadow bg-[#FBFBFB] hover:bg-[#eaeaea] transition-all duration-450 text-[#222222] flex items-center justify-center gap-1">
+        See More <GrFormNextLink className="text-[20px]" />
+      </button>
     </div>
   );
 }
