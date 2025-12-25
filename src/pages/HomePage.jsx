e2e1adafe3a5fecfdd239f7e0966061d3db95c47
ahmed-elmalah <o1027352352@gmail.com>
2025-12-25 11:41:04 +0200

    add circles imgs and apply main light bg color

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import OurCourses from "../Sections/OurCourses";
 
 export default function HomePage() {
   return (
-    <div className="w-full px-4 md:px-6 lg:px-17.75 py-2.5 relative dark:bg-black bg-white">
+    <div className="w-full px-4 md:px-6 lg:px-17.75 py-2.5 relative dark:bg-black bg-[#f4f5f4]">
       <NavBar />
       <HeroSection />
       <AboutUs />
