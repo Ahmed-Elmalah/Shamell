cd54eb6182cc5f5ec366ed1c1c28c3bcc18461de
ziad-mahdy <zyadmahdy01@gmail.com>
2025-12-25 18:35:18 +0200

    the last Final Meet Our Mentors Section part 3

--- src/Sections/MeetOurMentors.jsx
+++ src/Sections/MeetOurMentors.jsx
@@ -34,7 +34,7 @@ export default function MeetOurMentors() {
         },
         {
           MentorImg: MentorImg4,
-          title: "Ms. Omar Khaled   ",
+          title: "Ms. Sara Ahmed   ",
           icon:MentorIcon,
           content:
             "UI UX Designer"
